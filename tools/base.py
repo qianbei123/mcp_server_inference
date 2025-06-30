faf7c1302eb2be46a2c9720ca658a8a4e3415035
ShangChien <chenshangqian@dp.tech>
2025-06-30 16:01:23 +0800

    finish 3 core request func: ADMET, PK and Toxscan

--- tools/base.py
+++ tools/base.py
@@ -3,18 +3,21 @@ from typing import cast, Literal
 from structs.base import InputData, RES
 from structs.admet import AdmetRawResult, AdmetInnerData
 from structs.pk import InputPK,PKResult,ResponseData
+from structs.toxscan import ToxResult
+from httpx import Timeout
+DEFAULT_TIMEOUT = Timeout(60.0, connect=300.0)
 
 
-async def admet_predict(data: InputData) -> RES[AdmetInnerData]:
+async def ADMET_predict(data: InputData) -> RES[AdmetInnerData]:
     try:
-        async with httpx.AsyncClient() as client:
+        async with httpx.AsyncClient(timeout=DEFAULT_TIMEOUT) as client:
             url = "http://101.126.67.113:7893/admet/sync_mcp_admet_predict"
             response = await client.post(url, json={
                 'smiles': data.smiles,
             })
             response.raise_for_status()
-            res = cast(RES[AdmetRawResult], response.json())
-
+            res = RES[AdmetRawResult](**response.json())
+            
             if (
                 res.code != 0
                 or not res.data
@@ -36,7 +39,7 @@ async def Pharmacokinetics_predict(
     data: InputPK,
 ) -> RES[PKResult]:
     try:
-        async with httpx.AsyncClient() as client:
+        async with httpx.AsyncClient(timeout=DEFAULT_TIMEOUT) as client:
             url = "http://101.126.67.113:7893/pk/sync_mcp_pk_predict"
             response = await client.post(
                 url,
@@ -47,7 +50,8 @@ async def Pharmacokinetics_predict(
                 },
             )
             response.raise_for_status()
-            res = cast(RES[ResponseData], response.json())
+            res = RES[ResponseData](**response.json())
+
             if res.code != 0 or not res.data:
                 return RES(code=-1, msg=f"Pharmacokinetics predict error: {res.msg}")
             if not res.data.pk_result.data:
@@ -56,3 +60,29 @@ async def Pharmacokinetics_predict(
             return RES(data=res.data.pk_result.data)
     except Exception as e:
         return RES(code=-1, msg=f"Pharmacokinetics predict error: {e}")
+
+
+async def ToxScan_predict(data: InputData) -> RES[ToxResult]:
+    try:
+        async with httpx.AsyncClient(timeout=DEFAULT_TIMEOUT) as client:
+            url = "http://101.126.67.113:7893/funmg_api/task_mcp_sync/toxscan"
+
+            payload = {
+               "type": "smi",
+               "content": [
+                  data.smiles
+               ]
+            }
+            response = await client.post(url, json=payload)
+            response.raise_for_status()
+            res_raw = response.json()
+            res = RES[ToxResult](
+                code = res_raw['code'],
+                msg = res_raw['msg'],
+                data = res_raw['data'][0]
+            )
+
+            return res
+    except Exception as e:
+        return RES(code=-1, msg=f"tox predict error: {e}")
+
